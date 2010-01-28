124f2d0f547d58bbc5c788db37d1c2d15c6237ab
djemriza <djemriza@952686be-0be5-11df-a049-0325cb408e29>
2010-01-28 08:24:40 +0000

    Initial sources

---
+++ table-importer/src/tableimporter/Main.java
@@ -0,0 +1,20 @@
+/*
+ * File Name: Main.java
+ * @author djemriza
+ */
+
+package tableimporter;
+
+/**
+ *
+ * @author djemriza
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+
+}
