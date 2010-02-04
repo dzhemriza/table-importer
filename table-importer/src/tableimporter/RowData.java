d7c0b0704cd77300a9afa3d3d082930f98d5c52d
djemriza <djemriza@952686be-0be5-11df-a049-0325cb408e29>
2010-02-04 07:50:16 +0000

    Created class RowData and writing unit tests for it

---
+++ table-importer/src/tableimporter/RowData.java
@@ -0,0 +1,61 @@
+/*
+ * File Name: RowData.java
+ * @author djemriza
+ */
+
+package tableimporter;
+
+import java.util.TreeMap;
+import java.util.Iterator;
+import java.util.Set;
+import tableimporter.utils.CaseInsensitiveStringComparator;
+
+/**
+ * Class is responsible to hold row data that will be posted to destination
+ * table
+ * @author djemriza
+ */
+public class RowData {
+
+    private TreeMap<String, Object> mapDataHolder = new TreeMap<String, Object>(new CaseInsensitiveStringComparator());
+
+    public RowData() {
+    }
+
+    /**
+     * Method sets to some specific field value, if such field is not
+     * exists method creates new one
+     * @param strFieldName - field name
+     * @param objValue - field value
+     */
+    public void setFieldValue(String strFieldName, Object objValue) {
+        mapDataHolder.put(strFieldName, objValue);
+    }
+
+    /**
+     * Method gets for a specific field his value if there are no such field
+     * in the collection so returns null
+     * @param strFieldName - field name
+     * @return - if finds returns non null object
+     */
+    public Object getFieldValue(String strFieldName) {
+        return mapDataHolder.get(strFieldName);
+    }
+
+    /**
+     * Method returns a iterator to all fields in RowData
+     * @return
+     */
+    public Iterator<String> getFields() {
+        Set<String> keySet = mapDataHolder.keySet();
+        return keySet.iterator();
+    }
+
+    /**
+     * Method clears all row data
+     */
+    public void clearAll() {
+        mapDataHolder.clear();
+    }
+
+}
