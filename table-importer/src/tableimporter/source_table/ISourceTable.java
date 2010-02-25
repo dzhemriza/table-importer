d023a9f8fdc5e7aa80448756e714dd6fe2978c09
djemriza <djemriza@952686be-0be5-11df-a049-0325cb408e29>
2010-02-25 08:06:08 +0000

    Cosmetic changes

--- table-importer/src/tableimporter/source_table/ISourceTable.java
+++ table-importer/src/tableimporter/source_table/ISourceTable.java
@@ -5,10 +5,26 @@
 
 package tableimporter.source_table;
 
+import java.util.Iterator;
+import java.util.Vector;
+import tableimporter.collections.RowData;
+import tableimporter.fields.IField;
+
 /**
  *
  * @author djemriza
  */
 public interface ISourceTable {
 
+    /**
+     * Returns iterator to all containing data into source table
+     * @return
+     */
+    public Iterator<RowData> getRecords();
+
+    /**
+     * Returns a collection with description of all fields
+     * @return
+     */
+    public Vector<IField> getFields();
 }
