79b51baa8d665716388c6204649f4de05c2c6bb5
djemriza <djemriza@952686be-0be5-11df-a049-0325cb408e29>
2010-03-05 08:14:13 +0000

    Added RowImporter with abstract implementation Update IDestTable interface

--- table-importer/src/tableimporter/dest_table/IDestTable.java
+++ table-importer/src/tableimporter/dest_table/IDestTable.java
@@ -5,10 +5,25 @@
 
 package tableimporter.dest_table;
 
+import java.util.Vector;
+import tableimporter.fields.IField;
+import tableimporter.collections.RowData;
+
 /**
  *
  * @author djemriza
  */
 public interface IDestTable {
 
+    /**
+     * Method post current RowData into destination table
+     * @param rowData
+     */
+    public void postRecord(RowData rowData);
+
+    /**
+     * Returns a collection with description of all fields
+     * @return
+     */
+    public Vector<IField> getFields();
 }
