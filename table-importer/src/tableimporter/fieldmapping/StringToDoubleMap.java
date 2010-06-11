2842aa9a5572837212e86f0ff8c27173ef940192
djemriza <djemriza@952686be-0be5-11df-a049-0325cb408e29>
2010-06-11 07:07:35 +0000

    Issue 6: Create field mappings - StringToDate - StringToFloat - StringToDouble

---
+++ table-importer/src/tableimporter/fieldmapping/StringToDoubleMap.java
@@ -0,0 +1,43 @@
+/*
+ * table-importer
+ * Imports tabled data from any source to any destination
+ *
+ * File Name: StringToDoubleMap.java
+ *
+ * Copyright (C) 2010 Dzhem Riza
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package tableimporter.fieldmapping;
+
+import tableimporter.fields.IField;
+import tableimporter.fields.FieldType;
+
+/**
+ *
+ * @author djemriza
+ */
+public class StringToDoubleMap extends StringToTypeBase {
+
+    public StringToDoubleMap(IField srcField, IField dstField) {
+        super(srcField, dstField, FieldType.Double);
+    }
+
+    protected Object parseTypeFromString(String srcFieldValue) throws Exception {
+        double doubleVal = Double.parseDouble(srcFieldValue);
+        return doubleVal;
+    }
+
+}
