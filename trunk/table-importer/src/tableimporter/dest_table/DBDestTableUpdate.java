0eab3dea3026ac8f705ee69e5c24bed21e8762bf
djemriza <djemriza@952686be-0be5-11df-a049-0325cb408e29>
2010-07-22 07:56:15 +0000

    Removing ex.printStackTrace();

--- trunk/table-importer/src/tableimporter/dest_table/DBDestTableUpdate.java
+++ trunk/table-importer/src/tableimporter/dest_table/DBDestTableUpdate.java
@@ -59,8 +59,6 @@ public class DBDestTableUpdate extends DBDestTableBase {
         catch (SQLException ex) {
             logger.error(ex.getMessage());
             logger.debug("Full stack trace of exception:", ex);
-
-            ex.printStackTrace();
         }
     }
 }
